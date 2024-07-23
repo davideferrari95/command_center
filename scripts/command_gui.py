a9da3e99bcdc991d9b918fb812ebae3548ee4cc3
davideferrari95 <davideferr@unimore.it>
2024-07-23 16:52:38 +0200

    Update GUI

--- scripts/command_gui.py
+++ scripts/command_gui.py
@@ -38,143 +38,72 @@ class CommandGUI():
 
         # Labels
         self.labels : Dict[str, Label] = {}
-        default_labels = ['Name:', 'ID:', 'Type:', 'Info:']
-        command_labels = ['Direction', 'Distance:', 'Measure:', 'Location:', 'Object Name:', 'Object Location', 'From Location', 'To Location', 'Task Name', 'Status:', 'Command Info:']
-        label_texts = default_labels + command_labels
+        self.default_labels = ['Name:', 'ID:', 'Type:', 'Info:', 'Status:', 'Command Info:']
+        self.command_labels = ['Direction:', 'Distance:', 'Measure:', 'Location:', 'Object Name:', 'Object Location:', 'From Location:', 'To Location:', 'Task Name:']
+        label_texts = self.default_labels + self.command_labels
 
-        # for i, text in enumerate(label_texts):
-
-        #     # Font and Background Color - Dark and Light Grey Texts
-        #     label = Label(self.frame, text=text, bg='#F0F0F0', font=('Arial', 16, 'bold'), fg='#333')
-        #     self.labels[text] = Label(self.frame, text='Unknown', bg='#F0F0F0', font=('Arial', 16), fg='#666')
-
-        #     # Grid layout
-        #     label.grid(row=i, column=0, sticky='w', padx=10, pady=5)
-        #     self.labels[text].grid(row=i, column=1, sticky='w', padx=10, pady=5)
-
-        # Creating Labels
         for text in label_texts:
 
-            if text in ['Name:', 'ID:', 'Type:', 'Info:', 'Status:', 'Command Info:']:
+            # Create Default Labels
+            if text in self.default_labels:
 
                 # Font and Background Color - Dark and Light Grey Texts
                 label = Label(self.frame, text=text, bg='#F0F0F0', font=('Arial', 16, 'bold'), fg='#333')
-                self.labels[text] = Label(self.frame, text='Unknown', bg='#F0F0F0', font=('Arial', 16), fg='#666')
+                self.labels[text] = Label(self.frame, text='Unknown'.ljust(10), bg='#F0F0F0', font=('Arial', 16), fg='#666')
 
-                # Grid layout
-                if text == 'Name:':
-
-                    # Name - Row 0, Column 0,1
-                    label.grid(row=0, column=0, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=0, column=1, sticky='w', padx=50, pady=10)
-
-                elif text == 'ID:':
-
-                    # ID - Row 0, Column 2,3
-                    label.grid(row=0, column=2, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=0, column=3, sticky='w', padx=50, pady=10)
-
-                elif text == 'Type:':
-
-                    # Type - Row 0, Column 4,5
-                    label.grid(row=0, column=4, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=0, column=5, sticky='w', padx=50, pady=10)
-
-                elif text == 'Info:':
-
-                    # Info - Row 1, Column 0,1
-                    label.grid(row=1, column=0, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=1, column=1, sticky='w', padx=50, pady=5)
-
-                elif text == 'Status:':
-
-                    # Status - Row 6, Column 0,1
-                    label.grid(row=6, column=0, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=6, column=1, sticky='w', padx=50, pady=5)
-
-                elif text == 'Command Info:':
-
-                    # Command Info - Row 7, Column 0,1
-                    label.grid(row=7, column=0, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=7, column=1, sticky='w', padx=50, pady=5)
-
-            else:
+            # Create Command Labels
+            elif text in self.command_labels:
 
                 # Font and Background Color - Dark and Light Grey Texts
                 label = Label(self.frame, text=text, bg='#F0F0F0', font=('Arial', 12, 'bold'), fg='#333')
                 self.labels[text] = Label(self.frame, text='Unknown', bg='#F0F0F0', font=('Arial', 12), fg='#666')
 
-                # Grid layout
-                if text == 'Direction:':
+            # Grid - Forget Layout
+            label.grid_forget()
+            self.labels[text].grid_forget()
 
-                    # Direction - Row 3, Column 0,1
-                    label.grid(row=3, column=0, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=3, column=1, sticky='w', padx=50, pady=5)
-
-                elif text == 'Distance:':
-
-                    # Distance - Row 3, Column 2,3
-                    label.grid(row=3, column=2, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=3, column=3, sticky='w', padx=50, pady=5)
-
-                elif text == 'Measure:':
-
-                    # Measure - Row 3, Column 4,5
-                    label.grid(row=3, column=4, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=3, column=5, sticky='w', padx=50, pady=5)
-
-                elif text == 'Location:':
-
-                    # Location - Row 4, Column 0,1
-                    label.grid(row=4, column=0, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=4, column=1, sticky='w', padx=50, pady=5)                    
-
-                elif text == 'Object Name:':
-
-                    # Object Name - Row 3, Column 0,1
-                    label.grid(row=3, column=0, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=3, column=1, sticky='w', padx=50, pady=5)
-
-                elif text == 'Object Location:':
-
-                    # Object Location - Row 3, Column 2,3
-                    label.grid(row=3, column=2, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=3, column=3, sticky='w', padx=50, pady=5)
-
-                elif text == 'From Location:':
-
-                    # From Location - Row 3, Column 2,3
-                    label.grid(row=3, column=2, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=3, column=3, sticky='w', padx=50, pady=5)
-
-                elif text == 'To Location:':
-
-                    # To Location - Row 3, Column 4,5
-                    label.grid(row=3, column=4, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=3, column=5, sticky='w', padx=50, pady=5)
-
-                elif text == 'Task Name:':
-
-                    # Task Name - Row 3, Column 0,1
-                    label.grid(row=3, column=0, sticky='w', padx=10, pady=5)
-                    self.labels[text].grid(row=3, column=1, sticky='w', padx=50, pady=5)
+        # Create Initial Static Labels
+        Label(self.frame, text='Name:', bg='#F0F0F0', font=('Arial', 16, 'bold'), fg='#333').grid(row=0, column=0, sticky='w', padx=10, pady=10)
+        self.labels['Name:'].grid(row=0, column=1, sticky='w', padx=10, pady=10)
+        Label(self.frame, text='ID:', bg='#F0F0F0', font=('Arial', 16, 'bold'), fg='#333').grid(row=0, column=2, sticky='w', padx=10, pady=10)
+        self.labels['ID:'].grid(row=0, column=3, sticky='w', padx=10, pady=10)
+        Label(self.frame, text='Type:', bg='#F0F0F0', font=('Arial', 16, 'bold'), fg='#333').grid(row=0, column=4, sticky='w', padx=10, pady=10)
+        self.labels['Type:'].grid(row=0, column=5, sticky='w', padx=10, pady=10)
+        Label(self.frame, text='Info:', bg='#F0F0F0', font=('Arial', 16, 'bold'), fg='#333').grid(row=1, column=0, sticky='w', padx=10, pady=10)
+        self.labels['Info:'].grid(row=1, column=1, sticky='w', padx=10, pady=10)
+        Label(self.frame, text='Status:', bg='#F0F0F0', font=('Arial', 16, 'bold'), fg='#333').grid(row=6, column=0, sticky='w', padx=10, pady=10)
+        self.labels['Status:'].grid(row=6, column=1, sticky='w', padx=10, pady=10)
+        Label(self.frame, text='Command Info:', bg='#F0F0F0', font=('Arial', 16, 'bold'), fg='#333').grid(row=7, column=0, sticky='w', padx=10, pady=10)
+        self.labels['Command Info:'].grid(row=7, column=1, sticky='w', padx=10, pady=10)
 
         # Creating Spacing in Between Sections
         label = Label(self.frame, text=' ', bg='#F0F0F0', font=('Arial', 32, 'bold'), fg='#333')
-        label.grid(row=2, column=0, sticky='w', padx=10, pady=5)
+        label.grid(row=2, column=0, sticky='w')
         label = Label(self.frame, text=' ', bg='#F0F0F0', font=('Arial', 32, 'bold'), fg='#333')
-        label.grid(row=5, column=0, sticky='w', padx=10, pady=5)
+        label.grid(row=5, column=0, sticky='w')
 
-        # Initialize ROS Node
         rospy.init_node('command_gui', anonymous=True)
         rospy.Subscriber('alexa/command', MultimessageCommand, self.command_callback)
 
+    def hide_command_labels(self):
+
+        """ Hide Command Labels Helper Function """
+
+        # Hide Command Labels
+        for label in self.command_labels:
+            self.labels[label].grid_forget()
+
     def command_callback(self, msg:MultimessageCommand):
+
+        """ Command Callback Function """
+
+        # Hide Command Labels
+        self.hide_command_labels()
 
         # Get Command ID
         command_name, command_id, command_type = msg.name, msg.id, msg.type
         success, error_message = msg.success, msg.error_message
-        command = command_list.get_command_by_id(id)
+        command = command_list.get_command_by_id(command_id)
 
         # Hide all optional fields initially
         for text in ['Distance:', 'Location:', 'Measure:', 'Object Name:']:
@@ -195,6 +124,7 @@ class CommandGUI():
 
                     # GOTO Command Information
                     self.labels['Location:'].config(text=command.getLocation(), fg='#007BFF')
+                    self.labels['Location:'].grid(row=3, column=1, sticky='w')
 
                 else:
 
@@ -202,12 +132,17 @@ class CommandGUI():
                     self.labels['Direction:'].config(text=command.getDirection(), fg='#007BFF')
                     self.labels['Distance:'].config(text=command.getDistance(), fg='#007BFF')
                     self.labels['Measure:'].config(text=command.getMeasure(), fg='#007BFF')
+                    self.labels['Direction:'].grid(row=3, column=1, sticky='w')
+                    self.labels['Distance:'].grid(row=3, column=3, sticky='w')
+                    self.labels['Measure:'].grid(row=3, column=5, sticky='w')
 
             # Pick Command Information
             elif isinstance(command, PickCommand):
 
                 self.labels['Object Name:'].config(text=command.getObjectName(), fg='#007BFF')
                 self.labels['Location:'].config(text=command.getLocation(), fg='#007BFF')
+                self.labels['Object Name:'].grid(row=3, column=1, sticky='w')
+                self.labels['Location:'].grid(row=3, column=3, sticky='w')
 
             # Move Object Command Information
             elif isinstance(command, MoveObjectCommand):
@@ -215,22 +150,15 @@ class CommandGUI():
                 self.labels['Object Name:'].config(text=command.getObjectName(), fg='#007BFF')
                 self.labels['From Location:'].config(text=command.getFromLocation(), fg='#007BFF')
                 self.labels['To Location:'].config(text=command.getToLocation(), fg='#007BFF')
+                self.labels['Object Name:'].grid(row=3, column=1, sticky='w')
+                self.labels['From Location:'].grid(row=3, column=3, sticky='w')
+                self.labels['To Location:'].grid(row=3, column=5, sticky='w')
 
             # Execute Task Command Information
             elif isinstance(command, ExecuteTaskCommand):
 
-                self.labels['Task Name'].config(text=command.getTaskName(), fg='#007BFF')
-
-            else:
-
-                # self.labels['Direction:'].config(text='N/A', fg='#666')
-                self.labels['Distance:'].config(text='N/A', fg='#666')
-                self.labels['Measure:'].config(text='N/A', fg='#666')
-                self.labels['Location:'].config(text='N/A', fg='#666')
-                self.labels['Object Name:'].config(text='N/A', fg='#666')
-                self.labels['From Location:'].config(text='N/A', fg='#666')
-                self.labels['To Location:'].config(text='N/A', fg='#666')
-                self.labels['Task Name:'].config(text='N/A', fg='#666')
+                self.labels['Task Name:'].config(text=command.getTaskName(), fg='#007BFF')
+                self.labels['Task Name:'].grid(row=3, column=1, sticky='w')
 
             self.labels['Status:'].config(text='SUCCESS', fg='#007BFF')
             self.labels['Command Info:'].config(text='Command executed successfully', fg='#007BFF')
