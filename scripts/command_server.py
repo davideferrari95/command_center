cd852e87733598ea4ad85070e5c84698a97228ff
davideferrari95 <davideferr@unimore.it>
2024-07-25 14:12:19 +0200

    `return_function` and some fix

--- scripts/command_server.py
+++ scripts/command_server.py
@@ -3,6 +3,7 @@
 import sys, rospkg
 from flask import Flask, request, jsonify
 from termcolor import colored
+from typing import Union
 
 # Import Parent Folders
 sys.path.append(f'{rospkg.RosPack().get_path("alexa_conversation")}/AzureFunctions')
@@ -10,8 +11,23 @@ sys.path.append(f'{rospkg.RosPack().get_path("alexa_conversation")}/AzureFunctio
 # Import Commands
 from SkillBackend.Utils.command_list  import *
 
+# Command Union Type
+CommandUnion = Union[Command, PickCommand, MoveCommand, MoveObjectCommand, ExecuteTaskCommand]
+
 # Create the Flask App
 app = Flask(__name__)
+
+def return_function(command:CommandUnion, success:bool=False, json_status:int=400,  error_message:str='') -> jsonify:
+
+    """ Return Function to Send Command to ROS and JSON Response """
+
+    # Print Command Status
+    if success: print(colored('\nSUCCESS: ', 'green'), f'Feasible Command - {command.getName()}\n')
+    else: print(colored('\nERROR: ', 'red'), f'Infeasible Command - {command.getName()}\n', f'{error_message}\n')
+
+    # Return JSON Response
+    if success: return jsonify({'status': 'success', 'feasible': True}), json_status
+    else: return jsonify({'status': 'error', 'message': error_message}), json_status
 
 # Check Action Route
 @app.route('/check_action', methods=['POST'])
@@ -24,19 +40,25 @@ def check_action():
 
     # Check Data
     if not data or 'name' not in data or 'ID' not in data or 'type' not in data or 'info' not in data:
-        print(colored('\nERROR: ', 'red'), 'Invalid Input - Missing Data or Missing ID, Type, Name, or Info\n')
-        return jsonify({'status': 'error', 'message': 'Invalid input'}), 400
+
+        # Unknown Command Creation
+        command = Command('Unknown', 'Unknown', 'Unknown', 'Unknown') if not data else \
+                  Command('Unknown' if not 'name' in data else data['name'], 'Unknown' if not 'ID' in data else data['ID'],
+                          'Unknown' if not 'type' in data else data['type'], 'Unknown' if not 'info' in data else data['info'])
+
+        # Return Function
+        return return_function(command, False, 400, 'Invalid Input - Missing Data or Missing ID, Type, Name, or Info')
 
     # Check Command Type
     if not data['type'] in command_list.getCommandTypes():
-        print(colored('\nERROR: ', 'red'), 'Unknown Command Type\n')
-        return jsonify({'status': 'error', 'message': 'Unknown Command Type'}), 401
+
+        # Return Unknown Command Type
+        return return_function(Command(data['name'], data['ID'], data['type'], data['info']), False, 401, f'Unknown Command Type: {data["type"]}')
 
     if data['type'] in [NULL, ROS, DEFAULT]:
 
         # Return Feasibility True
-        print(colored('\nSUCCESS: ', 'green'), ' Feasible Command - NULL, ROS, or DEFAULT\n')
-        return jsonify({'status': 'success', 'feasible': True}), 200
+        return return_function(Command(data['name'], data['ID'], data['type'], data['info']), True, 200)
 
     # Check Movement Command Feasibility
     if data['type'] == MOVE:
@@ -51,8 +73,7 @@ def check_action():
 
         # Check Arguments
         if not all([key in data for key in ['direction', 'distance', 'measure']]) and not 'location' in data:
-            print(colored('\nERROR: ', 'red'), 'Invalid Input - Missing Direction, Distance, Measure, or Location\n')
-            return jsonify({'status': 'error', 'message': 'Invalid input'}), 400
+            return return_function(MoveCommand(data['name'], data['ID'], data['info']), False, 400, 'Invalid Input - Missing Direction, Distance, Measure, or Location')
 
         # Create Movement Command
         if 'location' in data: command = MoveCommand(data['name'], data['ID'], data['info'], location=data['location'])
@@ -60,8 +81,7 @@ def check_action():
 
         # TODO: Check Feasibility -> Add Some Logic Here
         # Check Available Locations...
-        print(colored('\nSUCCESS: ', 'green'), ' Feasible Command - Movement\n')
-        return jsonify({'status': 'success', 'feasible': True}), 200
+        return return_function(command, True, 200)
 
     # Check Stop Command Feasibility
     elif data['type'] == STOP:
@@ -70,8 +90,7 @@ def check_action():
         command = Command(data['name'], data['ID'], data['type'], data['info'])
 
         # TODO: Check Feasibility -> Add Some Logic Here
-        print(colored('\nSUCCESS: ', 'green'), ' Feasible Command - Stop\n')
-        return jsonify({'status': 'success', 'feasible': True}), 200
+        return return_function(command, True, 200)
 
     # Check Pick Command Feasibility
     elif data['type'] == PICK:
@@ -93,15 +112,13 @@ def check_action():
 
         # Check Arguments
         if not all([key in data for key in ['object_name', 'location']]):
-            print(colored('\nERROR: ', 'red'), 'Invalid Input - Missing Object Name or Location\n')
-            return jsonify({'status': 'error', 'message': 'Invalid input'}), 400
+            return return_function(PickCommand(data['name'], data['ID'], data['info']), False, 400, 'Invalid Input - Missing Object Name or Location')
 
         # Create Pick Command
         command = PickCommand(data['name'], data['ID'], data['info'], data['object_name'], data['location'])
 
         # TODO: Check Feasibility -> Add Some Logic Here
-        print(colored('\nSUCCESS: ', 'green'), ' Feasible Command - Pick\n')
-        return jsonify({'status': 'success', 'feasible': True}), 200
+        return return_function(command, True, 200)
 
     # Check Move Object Command Feasibility
     elif data['type'] == MOVE_OBJECT:
@@ -123,15 +140,13 @@ def check_action():
 
         # Check Arguments
         if not all([key in data for key in ['object_name', 'from_location', 'to_location']]):
-            print(colored('\nERROR: ', 'red'), 'Invalid Input - Missing Object Name, From Location, or To Location\n')
-            return jsonify({'status': 'error', 'message': 'Invalid input'}), 400
+            return return_function(MoveObjectCommand(data['name'], data['ID'], data['info']), False, 400, 'Invalid Input - Missing Object Name, From Location, or To Location')
 
         # Create Move Object Command
         command = MoveObjectCommand(data['name'], data['ID'], data['info'], data['object_name'], data['from_location'], data['to_location'])
 
         # TODO: Check Feasibility -> Add Some Logic Here
-        print(colored('\nSUCCESS: ', 'green'), ' Feasible Command - Move Object\n')
-        return jsonify({'status': 'success', 'feasible': True}), 200
+        return return_function(command, True, 200)
 
     # Check Execute Task Command Feasibility
     elif data['type'] == EXECUTE_TASK:
@@ -143,15 +158,13 @@ def check_action():
 
         # Check Arguments
         if not 'task_name' in data:
-            print(colored('\nERROR: ', 'red'), 'Invalid Input - Missing Task Name\n')
-            return jsonify({'status': 'error', 'message': 'Invalid input'}), 400
+            return return_function(ExecuteTaskCommand(data['name'], data['ID'], data['info']), False, 400, 'Invalid Input - Missing Task Name')
 
         # Create Execute Task Command
         command = ExecuteTaskCommand(data['name'], data['ID'], data['info'], data['task_name'])
 
         # TODO: Check Feasibility -> Add Some Logic Here
-        print(colored('\nSUCCESS: ', 'green'), ' Feasible Command - Execute Task\n')
-        return jsonify({'status': 'success', 'feasible': True}), 200
+        return return_function(command, True, 200)
 
 if __name__ == '__main__':
 
